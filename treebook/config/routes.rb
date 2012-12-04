0850236fc780487904f0397d7a07daaddd624b33
Eric Miller <Eric1868@gmail.com>
2012-12-04 10:00:37 -0500

    rails Basics - Install Devise, Add a User, and Install Devise Views

--- treebook/config/routes.rb
+++ treebook/config/routes.rb
@@ -1,6 +1,9 @@
 Treebook::Application.routes.draw do
-  resources :statuses
+  devise_for :users
 
+  resources :statuses
+  root :to => "statuses#index"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
